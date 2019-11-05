d6c4e6cfb72b3f19186f96957a18694211a00d3f
Hayden Kua <kua.h@etoncollege.org.uk>
2019-11-05 11:11:50 +0000

    Initial check-in of code

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Kua, Hayden (NA) on 05/11/2019.
+//  Copyright © 2019 Kua, Hayden (NA). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let name = "Hayden Kua"
+
+print(name)
+
+//this is some comment
